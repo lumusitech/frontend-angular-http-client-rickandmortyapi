da4a98ee857f7ef445cab56bd3450051bb21c826
lumusitech <lumusitech@gmail.com>
2023-11-12 18:59:38 -0300

    feat: add interfaces and fix types in service

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -1,42 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { CharacterService } from '../character.service';
+import { APIResponse, Result } from '../interfaces/api-response.interface';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
-  styleUrls: ['./character.component.scss']
+  styleUrls: ['./character.component.scss'],
 })
 export class CharacterComponent implements OnInit {
   page: number;
   nroPages: number;
-  response: any[];
-  characters: any[];
+  response?: APIResponse;
+  characters?: Result[];
   constructor(private characterService: CharacterService) {
     this.page = 1;
     this.nroPages = 0;
-    this.response = [];
-    this.characters = [];
   }
 
-  async ngOnInit(): Promise<any> {
+  async ngOnInit() {
     this.response = await this.characterService.getListByPage(this.page);
-    this.characters = this.response.results;
-    this.nroPages = this.response.info.pages;
+
+    if (this.response) {
+      this.characters = this.response?.results;
+      this.nroPages = this.response.info.pages;
+    }
   }
 
   async onClickPrev() {
     if (this.page === 1) this.page = this.nroPages;
     else this.page--;
     this.response = await this.characterService.getListByPage(this.page);
-    this.characters = this.response.results;
+    this.characters = this.response?.results;
   }
 
   async onClickNext() {
     if (this.page === this.nroPages) this.page = 1;
     else this.page++;
     this.response = await this.characterService.getListByPage(this.page);
-    this.characters = this.response.results;
+    this.characters = this.response?.results;
   }
-
-
 }
